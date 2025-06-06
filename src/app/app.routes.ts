09234d3c732eee25ce37dfba879256074b7a0340
tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-06 23:06:38 +0700

    3-5 real moi update 3-5 con lai khong can vaf 3-5 dau tien laf 3-4

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CategoryList } from './features/category/category-list/category-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//them duong dan link category list
+{
+    path:'admin/categories',
+    component: CategoryList
+}
+
+];
