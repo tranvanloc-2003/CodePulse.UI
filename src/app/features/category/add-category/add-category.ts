031131628a945c529a16e9bf91d060d05507009c
tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-07 01:27:15 +0700

    3-9 lam them viec huy dang ki cho add category vaf 3-8 o ben API (cong khai api)

--- src/app/features/category/add-category/add-category.ts
+++ src/app/features/category/add-category/add-category.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AddCategoryRequest } from '../models/add-category-request-models';
 import { HttpClientModule } from '@angular/common/http';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
-  imports: [FormsModule,HttpClientModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './add-category.html',
   styleUrl: './add-category.css'
 })
-export class AddCategory {
+export class AddCategory implements OnDestroy {
+
   model: AddCategoryRequest;
+  private addCategorySubscribtion?: Subscription
   constructor(private categoryService: CategoryService) {
     this.model = {
       name: '',
@@ -19,16 +22,20 @@ export class AddCategory {
     };
   }
 
-  onFormSubmit() {
-// console.log(this.model);//check form
-this.categoryService.addCategory(this.model).subscribe({
-  next: (response) =>{
-console.log('Ket noi thanh cong');
-  },
-  error: (error) =>{
-console.log('Ket noi that bai');
-  }
-})
 
+  onFormSubmit() {
+    // console.log(this.model);//check form
+    this.addCategorySubscribtion = this.categoryService.addCategory(this.model).subscribe({
+      next: (response) => {
+        console.log('Ket noi thanh cong');
+      },
+      error: (error) => {
+        console.log('Ket noi that bai');
+      }
+    })
+
+  }
+  ngOnDestroy(): void {
+    this.addCategorySubscribtion?.unsubscribe();
   }
 }
