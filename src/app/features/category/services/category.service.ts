1b90d670bc0942f45934b3e9ede8c18aab2f1942
tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-07 16:57:51 +0700

    4-4 them environment bang lenh ng generate environments va bo duong dan  localhost vao environment

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { AddCategoryRequest } from '../models/add-category-request-models';
 import { HttpClient } from '@angular/common/http';
 import { CategoryModels } from '../models/category.model';
+import { environment } from '../../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
@@ -14,10 +15,10 @@ export class CategoryService {
   }
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>(`https://localhost:7153/api/categories`, model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
   }
   //categoryModels[] lay theo mang
   getAllCategory(): Observable<CategoryModels[]> {
-   return this.http.get<CategoryModels[]>(`https://localhost:7153/api/Categories`);
+   return this.http.get<CategoryModels[]>(`${environment.apiBaseUrl}/api/Categories`);
   }
 }
