7d7c39d8666ef72bf4d1746d100de0bb6031dee0
tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-07 16:40:38 +0700

    4-3 them thieu

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AddCategoryRequest } from '../models/add-category-request-models';
 import { HttpClient } from '@angular/common/http';
+import { CategoryModels } from '../models/category.model';
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +14,10 @@ export class CategoryService {
   }
 
   addCategory(model: AddCategoryRequest): Observable<void> {
-    return this.http.post<void>(`https://localhost:7153/api/categories`,model);
+    return this.http.post<void>(`https://localhost:7153/api/categories`, model);
+  }
+  //categoryModels[] lay theo mang
+  getAllCategory(): Observable<CategoryModels[]> {
+   return this.http.get<CategoryModels[]>(`https://localhost:7153/api/Categories`);
   }
 }
