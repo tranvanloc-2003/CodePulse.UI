52960928d5227d5aa843305257fc9b5a7c112796
tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-07 17:16:10 +0700

    3-5 viet code api hien len man hinh

--- src/app/features/category/category-list/category-list.ts
+++ src/app/features/category/category-list/category-list.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CategoryService } from '../services/category.service';
+import { CategoryModels } from '../models/category.model';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-category-list',
-  imports: [RouterLink],
+  imports: [RouterLink,NgIf,NgFor],
   templateUrl: './category-list.html',
   styleUrl: './category-list.css'
 })
-export class CategoryList {
+export class CategoryList implements OnInit {
+  constructor(private categoryService: CategoryService) {
 
+  }
+  categories?: CategoryModels[];
+  ngOnInit(): void {
+    this.categoryService.getAllCategory().subscribe({
+      next: (response) => {
+        this.categories = response;
+      },
+      error: (err) => {
+
+      },
+    });
+  }
 }
