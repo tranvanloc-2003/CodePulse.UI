5153a1e32bda36f8696f27eccda699a7b46b1a1e
Tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-10 23:12:34 +0700

    10-5 hoan thanh viec upload thuoc tinh categories len ui blogpost

--- src/app/features/blog-post/models/blog-post-models.ts
+++ src/app/features/blog-post/models/blog-post-models.ts
@@ -1,3 +1,5 @@
+import { CategoryModels } from "../../category/models/category.model";
+
 export interface BlogPost {
     id: string;
     title: string;
@@ -8,4 +10,5 @@ export interface BlogPost {
     publishedDate: Date;
     author: string;
     isvisible: boolean;
+    categories: CategoryModels[];
 }
