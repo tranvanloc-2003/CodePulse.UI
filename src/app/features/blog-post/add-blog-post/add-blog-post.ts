27b50f5abed66507398a4902b63f6dc45f5fa11a
Tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-06-15 17:51:54 +0700

    13-12 hoan thanh chuc nang selectedimage duoc copy tu edit blogpost sang

--- src/app/features/blog-post/add-blog-post/add-blog-post.ts
+++ src/app/features/blog-post/add-blog-post/add-blog-post.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Title } from '@angular/platform-browser';
-import { AsyncPipe, DatePipe, NgFor, NgIf } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass, NgFor, NgIf } from '@angular/common';
 import { BlogpostServices } from '../services/blogpost.services';
 import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
@@ -9,18 +9,21 @@ import { AddBlogPostRequest } from '../models/add-blog-post-request';
 import { MarkdownModule } from 'ngx-markdown';
 import { CategoryService } from '../../category/services/category.service';
 import { CategoryModels } from '../../category/models/category.model';
+import { ImageSelector } from "../../../shared/components/image-selector/image-selector";
+import { ImageServices } from '../../../shared/components/image-selector/services/image.services';
 
 @Component({
   selector: 'app-add-blog-post',
-  imports: [FormsModule, DatePipe, MarkdownModule, NgIf, AsyncPipe,NgFor],
+  imports: [FormsModule, DatePipe, MarkdownModule, NgIf, AsyncPipe, NgFor, ImageSelector, NgClass],
   templateUrl: './add-blog-post.html',
   styleUrl: './add-blog-post.css'
 })
-export class AddBlogPost implements OnInit {
+export class AddBlogPost implements OnInit, OnDestroy {
   model: AddBlogPostRequest;
   categories$?: Observable<CategoryModels[]>;
-  
-  constructor(private blogpostServices: BlogpostServices, private router: Router, private categoryServices: CategoryService) {
+  isImageSelectorVisible: boolean = false;
+    imageSelectSubscription?: Subscription;
+  constructor(private blogpostServices: BlogpostServices, private router: Router, private categoryServices: CategoryService, private imageServices :ImageServices) {
     this.model = {
       title: '',
       shortDescription: '',
@@ -33,7 +36,7 @@ export class AddBlogPost implements OnInit {
       categories: []
     }
   }
-
+ 
   onFormSubmit(): void {
     console.log(this.model);
     this.blogpostServices.addBlogPost(this.model).subscribe({
@@ -42,8 +45,24 @@ export class AddBlogPost implements OnInit {
       }
     })
   }
-
+  //mo image selector
+  openImageSelector(): void {
+    this.isImageSelectorVisible = true;
+  }
+  closeImageSelector(): void {
+    this.isImageSelectorVisible = false;
+  }
   ngOnInit(): void {
     this.categories$ = this.categoryServices.getAllCategory();
+  this.imageSelectSubscription=   this.imageServices.onSelectImage().subscribe({
+      next: (image) => {
+        this.model.featuredImageUrl = image.url;
+        this.isImageSelectorVisible = false; // Close the image selector after selecting an image
+      }
+    });
+  } 
+  ngOnDestroy(): void {
+    this.imageSelectSubscription?.unsubscribe();
   }
+
 }
