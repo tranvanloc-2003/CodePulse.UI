07471a08932cf5acbd11d196dec1f825b9ad8afb
Tranvanloc-2003 <tranvanlocwork@gmail.com>
2025-07-01 17:30:53 +0700

    15-20 fix tuong tu loi 401 trong edit blospost

--- src/app/features/blog-post/services/blogpost.services.ts
+++ src/app/features/blog-post/services/blogpost.services.ts
@@ -5,13 +5,14 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { AddBlogPostRequest } from '../models/add-blog-post-request';
 import { BlogPost } from '../models/blog-post-models';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogpostServices {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookiesServices: CookieService) { }
   addBlogPost(data: AddBlogPostRequest): Observable<BlogPost> {
     return this.http.post<BlogPost>(`${environment.apiBaseUrl}/api/BlogPost`, data);
   }
@@ -22,7 +23,14 @@ export class BlogpostServices {
     return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/blogpost/${id}`);
   }
   updateBlogPost(id: string, data: AddBlogPostRequest): Observable<BlogPost> {
-    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/BlogPost/${id}`, data);
+    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/BlogPost/${id}`, data,
+
+      {
+        headers:{
+          'Authorization': this.cookiesServices.get('Authorization')
+        }
+      }
+    );
   }
   deleteBlogPost(id: string):Observable<BlogPost>{
     return this.http.delete<BlogPost>(`${environment.apiBaseUrl}/api/BlogPost/${id}`);
